4a8b4b52dd030e4cb46ec67dd34964cdde3fde22
Tawan <ntctawan@gmail.com>
2026-04-16 17:25:07 -0300

    direcionamento de login para o menu

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -56,9 +56,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'core',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -141,3 +143,5 @@ import os
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CORS_ALLOW_ALL_ORIGINS = True
